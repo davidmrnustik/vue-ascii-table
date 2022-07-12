21b83832586814f931658202a117483808880b86
David Mrnustik <david.mrnustik@hotmail.com>
2022-07-12 10:09:50 +0200

    refactor: complete refactor with vue 2 + vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <h1>{{title}}</h1>
+  <v-app>
+    <input v-model="tableFormat" @change="onChange" type="number" min="1" max="4" placeholder="Type a table format">
+    <p>Format is: <strong>{{tableFormat}}</strong></p>
 
-  <input v-model="tableFormat" @change="onChange" type="number" min="1" max="4" placeholder="Type a table format">
-  <p>Format is: <strong>{{tableFormat}}</strong></p>
-  {{tableData}}
+    <v-data-table
+        v-if="tableData"
+        :headers="getHeaders"
+        :items="getTableData"
+        :items-per-page="127"
+        item-key="name"
+        class="elevation-1"
+        :search="search"
+    >
+      <template v-slot:top>
+        <v-text-field
+            v-model="search"
+            label="Search"
+            class="mx-4"
+        ></v-text-field>
+    </template>
 
+    </v-data-table>
+  </v-app>
 </template>
 
 <script>
-import module from '../public/a.out.js';
+import module from './assets/js/a.out.js';
 
 let instance = {
   ready: new Promise(resolve => {
@@ -23,25 +40,78 @@ let instance = {
   })
 };
 
-
 export default {
+  name: 'App',
+
   data() {
     return {
       tableFormat: 4,
       title: "Ascii table",
-      tableData: null
+      tableData: [],
+      search: ""
+    }
+  },
+  computed: {
+    getHeaders() {
+      return this.tableData[0];
+    },
+    getTableData() {
+      return this.tableData[1]
     }
   },
   methods: {
     onChange(e) {
       this.getTable(e.target.value);
     },
-    getTable(value) {
-      instance.ready.then(_ => this.tableData = instance.getTable(value));
+    prepareData(data) {
+      // [["Dec", "Oct", "Bin"], [0, "x80", "00000000"]]
+      // [{ text: "Dec", value: "dec1" }, { text: "Oct", value: "oct1" }, { text: "Bin", value: "bin1" }]
+      // [{ dec1: 0 }, { oct1: "x80" }, { bin1: "00000000" }]
+      /*
+      [
+        [{ text: "Dec", value: "dec1" }, { text: "Oct", value: "oct1" }, { text: "Bin", value: "bin1" }],
+        [{ dec1: 0, oct1: "x80", bin1: "00000000" }, { dec1: 1, oct1: "x81", bin1: "00000001" }, ]
+      ]
+       */
+      const parsed = JSON.parse(data);
+      let headerData = []
+      const cellData = []
+
+      parsed.forEach((row, rIdx) => {
+        if (rIdx === 0) {
+          headerData = row.map((col, cIdx) => {
+            return {
+              text: col,
+              sortable: false,
+              value: `${col.toLowerCase()}${cIdx}`
+            }
+          })
+        } else {
+          const res = row.reduce((acc, cur, index) => {
+            return {
+              ...acc,
+              [`${(parsed[0][index]).toLowerCase()}${index}`]: cur
+            }
+          }, {})
+          cellData.push(res)
+        }
+      })
+
+      return [headerData, cellData]
     },
+    getTable(value) {
+      instance.ready.then(() => this.tableData = this.prepareData(instance.getTable(value)));
+    },
+    filterOnlyDec(value, search) {
+      console.log("value", value)
+      console.log("search", search)
+      return value != null &&
+          search != null &&
+          value.indexOf(search) !== -1
+    }
   },
   mounted() {
     this.getTable(this.tableFormat);
   }
-}
+};
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,7 @@
-// Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/';
 
-// Vuetify
-import { createVuetify } from 'vuetify'
+Vue.use(Vuetify);
 
-export default createVuetify({
-  components,
-  directives
-})
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default new Vuetify({
+});
